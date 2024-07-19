c1968483c3ffe3c714476bfe1316bbafad17da2f
dragonlongago <129839439+dragonlongago@users.noreply.github.com>
2024-07-20 01:28:31 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -221,6 +221,7 @@ const openaiModels = [
   "gpt-4-vision-preview",
   "gpt-4-turbo-2024-04-09",
   "gpt-4-1106-preview",
+  "claude-3-5-sonnet-20240620",
 ];
 
 const googleModels = [
@@ -237,7 +238,6 @@ const anthropicModels = [
   "claude-3-sonnet-20240229",
   "claude-3-opus-20240229",
   "claude-3-haiku-20240307",
-  "claude-3-5-sonnet-20240620",
 ];
 
 const baiduModels = [
