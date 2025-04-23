77ca768f8a7686294a70f092440052fc1c7b4fb2
dragonlongago <129839439+dragonlongago@users.noreply.github.com>
2025-04-23 23:35:18 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -501,7 +501,7 @@ const openaiModels = [
   "gemini-2.0-flash-thinking-exp-1219",
   "gemini-2.0-flash-thinking-exp-01-21",
   "gemini-2.0-pro-exp",
-  "gemini-2.0-pro-exp-02-05",
+  "gemini-2.5-pro-exp-03-25",
   // "gpt-4",
   // "gpt-4-0613",
   // "gpt-4-32k",
